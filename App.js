a96ab721506eb0e377dc8ed3eaddbabf62f7cd58
unknown <nikkirohilla394@gmail.com>
2023-12-19 17:34:36 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+// const heading=React.createElement("h1",{id:"heading"},"Hello World from react!");
+//     const root=ReactDOM.createRoot(document.getElementById("root"));
+//     root.render(heading);
+/**
+ * 
+ * <div id="parent">
+ * <div id="child">
+ * <h1>I'm a h1 tag
+ * </h1>
+ * 
+ * </div>
+ * 
+ * </div>
+ *
+ */
+const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"}
+,[React.createElement("h1",{},"I'm an h1 tag"),
+React.createElement("h1",{},"I'm an h1 tag"),
+React.createElement("h2",{},"I am an h2 tag"),
+
+React.createElement("h2",{},"I am an h2 tag")]));
+ 
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+ i
