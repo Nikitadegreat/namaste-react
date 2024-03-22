ba53e4f4b8129fe94dc3926a85822384ed86dc0a
unknown <nikkirohilla394@gmail.com>
2024-03-22 09:15:53 +0530

    episode-01

--- App.js
+++ App.js
@@ -1,25 +1,4 @@
-// const heading=React.createElement("h1",{id:"heading"},"Hello World from react!");
-//     const root=ReactDOM.createRoot(document.getElementById("root"));
-//     root.render(heading);
-/**
- * 
- * <div id="parent">
- * <div id="child">
- * <h1>I'm a h1 tag
- * </h1>
- * 
- * </div>
- * 
- * </div>
- *
- */
-const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"}
-,[React.createElement("h1",{},"I'm an h1  tag"),  
-React.createElement("h1",{},"I'm an h1 tag"),
-React.createElement("h2",{},"I am an h2 tag"),
-
-React.createElement("h2",{},"I am an h2 tag")]));
- 
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
- i
+const heading=React.createElement("h1",
+{id:"heading",xyz:"abc"},"Hello world from react!");
+        const root=ReactDOM.createRoot(document.getElementById("root"));
+        root.render(heading);
